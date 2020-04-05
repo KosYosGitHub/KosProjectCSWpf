57d9f7c73e29aef8e20dd69c97b00c875cd64833
KosYosGitHub <kosyos_address@yahoo.co.jp>
2020-04-05 17:45:01 +0900

    NamedAPIResourceをフォルダ化、Languageファイルだけ追加

---
+++ PokeAPI/Utility/Language/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KosMVVM;
+
+namespace PokeAPI
+{
+	/// <summary>
+	/// 言語
+	/// </summary>
+	public class Language : KosViewModel
+	{
+	}
+}
