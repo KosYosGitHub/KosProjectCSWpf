29bc1cd6e19a404257df1074afdbeb3906c6d31b
KosYosGitHub <kosyos_address@yahoo.co.jp>
2020-05-17 09:31:45 +0900

    コメント追加、マジックナンバーの記号定数化

--- KosID3Tag/ID3TagV1Model.cs
+++ KosID3Tag/ID3TagV1Model.cs
@@ -14,7 +14,12 @@ namespace KosID3Tag
 		internal bool IsExists { get; private set; } = false;
 		#endregion
 
+		#region バージョン
+		/// <summary>
+		/// バージョン
+		/// </summary>
 		internal ID3TagV1Version Version { get; set; } = ID3TagV1Version.ID3v1;
+		#endregion
 
 		#region 曲名
 		/// <summary>
@@ -82,47 +87,46 @@ namespace KosID3Tag
 		}
 		#endregion
 
-		// public メソッド
+		// internal メソッド
 
 		#region ファイルの読込
 		/// <summary>
 		/// ファイルの読込
 		/// </summary>
 		/// <param name="path">読込ファイルパス</param>
-		public void ReadFile(string path)
+		internal void ReadFile(string path)
 		{
-			const int id3TagLength = 128;
-			byte[] data = new byte[id3TagLength];
+			byte[] data = new byte[LengthTotal];
 
 			// ファイルの末尾128byte分を読込
 			using(FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
-				long startPos = fs.Length - id3TagLength;
+				long startPos = fs.Length - LengthTotal;
 
 				fs.Seek(startPos, SeekOrigin.Begin);
-				fs.Read(data, 0, id3TagLength);
+				fs.Read(data, 0, LengthTotal);
 			}
 
 			Encoding shiftJIS = Encoding.GetEncoding("Shift-JIS");
 
 			// 先頭3byteが"TAG"
-			if(shiftJIS.GetString(data, 0, 3) == "TAG") {
+			if(shiftJIS.GetString(data, OffsetHeader, LengthHeader) == "TAG") {
 				IsExists = true;
-				SongTitle = shiftJIS.GetString(data, 3, 30).TrimEnd('\0');		// 曲名
-				Artist = shiftJIS.GetString(data, 33, 30).TrimEnd('\0');        // タイトル
-				Album = shiftJIS.GetString(data, 63, 30).TrimEnd('\0');         // アルバム
-				Year = shiftJIS.GetString(data, 93, 4).TrimEnd('\0');           // 西暦
+				SongTitle = shiftJIS.GetString(data, OffsetSongTitle, LengthSongTitle).TrimEnd('\0');		// 曲名
+				Artist = shiftJIS.GetString(data, OffsetArtist, LengthArtist).TrimEnd('\0');				// アーティスト
+				Album = shiftJIS.GetString(data, OffsetAlbum, LengthAlbum).TrimEnd('\0');					// アルバム
+				Year = shiftJIS.GetString(data, OffsetYear, LengthYear).TrimEnd('\0');						// 西暦
 				if(data[125] != 0) {
 					// ID3 v1
 					Version = ID3TagV1Version.ID3v1;
-					Comment = shiftJIS.GetString(data, 97, 30).TrimEnd('\0');	// コメント
+					Comment = shiftJIS.GetString(data, OffsetComment, LengthCommentV1).TrimEnd('\0');		// コメント
 				}
 				else {
 					// ID3 v1.1
 					Version = ID3TagV1Version.ID3v1_1;
-					Comment = shiftJIS.GetString(data, 97, 27).TrimEnd('\0');   // コメント
-					TrackNo = data[126];
+					Comment = shiftJIS.GetString(data, OffsetComment, LengthCommentV1_1).TrimEnd('\0');		// コメント
+					TrackNo = data[OffsetTrackNo];
 				}
-				GenreNo = data[127];
+				GenreNo = data[OffsetGenreNo];																// ジャンル番号
 			}
 			else {
 				IsExists = false;
@@ -136,6 +140,120 @@ namespace KosID3Tag
 				GenreNo = 0;
 			}
 		}
+		#endregion
+
+		// private 定数
+
+		#region ヘッダ位置
+		/// <summary>
+		/// ヘッダ位置
+		/// </summary>
+		private const int OffsetHeader = 0;
+		#endregion
+
+		#region 曲名位置
+		/// <summary>
+		/// 曲名位置
+		/// </summary>
+		private const int OffsetSongTitle = 3;
+		#endregion
+
+		#region アーティスト位置
+		/// <summary>
+		/// アーティスト位置
+		/// </summary>
+		private const int OffsetArtist = 33;
+		#endregion
+
+		#region アルバム位置
+		/// <summary>
+		/// アルバム位置
+		/// </summary>
+		private const int OffsetAlbum = 63;
+		#endregion
+
+		#region 西暦位置
+		/// <summary>
+		/// 西暦位置
+		/// </summary>
+		private const int OffsetYear = 93;
+		#endregion
+
+		#region コメント位置
+		/// <summary>
+		/// コメント位置
+		/// </summary>
+		private const int OffsetComment = 97;
+		#endregion
+
+		#region トラック番号
+		/// <summary>
+		/// トラック番号
+		/// </summary>
+		private const int OffsetTrackNo = 126;
+		#endregion
+
+		#region ジャンル番号
+		/// <summary>
+		/// ジャンル番号
+		/// </summary>
+		private const int OffsetGenreNo = 127;
+		#endregion
+
+		#region 総サイズ
+		/// <summary>
+		/// 総サイズ
+		/// </summary>
+		private const int LengthTotal = 128;
+		#endregion
+
+		#region ヘッダサイズ
+		/// <summary>
+		/// ヘッダサイズ
+		/// </summary>
+		private const int LengthHeader = 3;
+		#endregion
+
+		#region 曲名サイズ
+		/// <summary>
+		/// 曲名サイズ
+		/// </summary>
+		private const int LengthSongTitle = 30;
+		#endregion
+
+		#region アーティストサイズ
+		/// <summary>
+		/// アーティストサイズ
+		/// </summary>
+		private const int LengthArtist = 30;
+		#endregion
+
+		#region アルバムサイズ
+		/// <summary>
+		/// アルバムサイズ
+		/// </summary>
+		private const int LengthAlbum = 30;
+		#endregion
+
+		#region 西暦サイズ
+		/// <summary>
+		/// 西暦サイズ
+		/// </summary>
+		private const int LengthYear = 4;
+		#endregion
+
+		#region コメント(v1)サイズ
+		/// <summary>
+		/// コメント(v1)サイズ
+		/// </summary>
+		private const int LengthCommentV1 = 30;
+		#endregion
+
+		#region コメント(v1.1)サイズ
+		/// <summary>
+		/// コメント(v1.1)サイズ
+		/// </summary>
+		private const int LengthCommentV1_1 = 28;
 		#endregion
 	}
 }
